e3c4182a83505e9405953a4c2afb4d9417c34c98
EileenW <yulin1393@hotmail.com>
2016-05-12 16:06:07 -0400

    no message

---
+++ app/src/main/java/kuma/github/com/grocerymanager/MainActivity.java
@@ -0,0 +1,14 @@
+package kuma.github.com.grocerymanager;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+public class MainActivity extends ActionBarActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
