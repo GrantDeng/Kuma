556c0aea836ae73ad57142fa90fa18531c1a23ef
Bob W. Hogg <rwhogg@linux.com>
2016-06-10 23:23:54 -0400

    more javadoc

--- app/src/main/java/com/github/kuma/data/db/ViewUtils.java
+++ app/src/main/java/com/github/kuma/data/db/ViewUtils.java
@@ -7,13 +7,23 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.Map;
 
+/**
+ * ViewUtils is a utility class for handling Couchbase Views.
+ */
 public class ViewUtils
 {
     private ViewUtils() {}
 
+    /**
+     * Retrieves a JavaBean-style setter method for a given class that acts on a certain field.
+     * Necessary because Android does not support the full java.beans package.
+     * @param klass The class with the setter method.
+     * @param fieldType The field the setter acts on.
+     * @return The setter method.
+     * @throws NoSuchMethodException
+     */
     private static Method getSetterMethod(Class<?> klass, Field fieldType) throws NoSuchMethodException
     {
-        // java.beans support is not available on Android :(
         String fieldName = fieldType.getName();
         String capitalized = Character.toUpperCase(fieldName.charAt(0)) + fieldName.substring(1);
         for(Method method: klass.getMethods())
@@ -23,11 +33,24 @@ public class ViewUtils
         return klass.getMethod("set" + capitalized, fieldType.getType());
     }
 
+    /**
+     * Return a Mapper (as in MapReduce) that maps names (document ID's) to Objects of the given type.
+     * @param klass Type of the result. Also filters the data we are interested in to just that of the given type.
+     * @return The created Mapper.
+     * @throws ClassNotFoundException
+     */
     public static Mapper typeMapper(final Class<?> klass) throws ClassNotFoundException
     {
         return ViewUtils.typeMapper(klass, klass.getDeclaredFields());
     }
 
+    /**
+     * Return a Mapper (as in MapReduce) that maps names (document ID's) to Objects of the given type.
+     * Only the fields we are interested in are filled in.
+     * @param klass Type of the result. Also filters the data we are interested in to just that of the given type.
+     * @param fields The fields we are interested in.
+     * @return The created Mapper.
+     */
     public static Mapper typeMapper(final Class<?> klass, final Field[] fields)
     {
         return new Mapper()
