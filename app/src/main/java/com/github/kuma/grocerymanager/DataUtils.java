5bb34a6e63dad0fb96ab0e974a2c8d3d810e8803
Bob W. Hogg <rwhogg@linux.com>
2016-06-23 15:33:05 -0400

    Add DataUtils.getByName()

--- app/src/main/java/com/github/kuma/grocerymanager/DataUtils.java
+++ app/src/main/java/com/github/kuma/grocerymanager/DataUtils.java
@@ -1,6 +1,12 @@
 package com.github.kuma.grocerymanager;
 
+import com.couchbase.lite.CouchbaseLiteException;
+import com.couchbase.lite.QueryEnumerator;
+import com.couchbase.lite.View;
+import com.github.kuma.data.db.CouchbaseHandler;
 import com.github.kuma.db_object.Data;
+
+import java.io.IOException;
 
 /**
  * Utilities for working with the Data class
@@ -12,11 +18,25 @@ public final class DataUtils
     /**
      * Retrieve a Data object by its name.
      * @param name The name of the desired Data object.
+     * @param handler Couchbase handler to use.
      * @return The object if it exists, null otherwise.
+     * @throws CouchbaseLiteException
+     * @throws IOException
+     * @throws ClassNotFoundException
      */
-    public static Data getByName(String name)
+    public static Data getByName(String name, CouchbaseHandler handler) throws CouchbaseLiteException, IOException,
+        ClassNotFoundException
     {
-        // FIXME stub
+        View dataView = AvailableViews.getDataView(handler);
+        QueryEnumerator queryEnumerator = dataView.createQuery().run();
+        while(queryEnumerator.hasNext())
+        {
+            Data data = (Data) queryEnumerator.next().getValue();
+            if(data.getName().equals(name))
+            {
+                return data;
+            }
+        }
         return null;
     }
 }
