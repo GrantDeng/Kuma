570ca9a206236fb358bee713b0a1a78721c68fab
EileenW <yulin1393@hotmail.com>
2016-06-27 19:00:56 -0400

    move getDaysBetween to DateUtils

--- app/src/main/java/com/github/kuma/grocerymanager/DateUtils.java
+++ app/src/main/java/com/github/kuma/grocerymanager/DateUtils.java
@@ -1,5 +1,8 @@
 package com.github.kuma.grocerymanager;
 
+import org.joda.time.DateTime;
+import org.joda.time.Days;
+
 import java.util.Calendar;
 import java.util.Date;
 
@@ -20,5 +23,22 @@ public class DateUtils
         final Calendar calendar = Calendar.getInstance();
         calendar.set(year, month, day);
         return calendar.getTime();
+    }
+
+    /**
+     * Utility method to count number of days between two Date objects
+     * @param firstdate
+     * @param seconddate
+     * @return
+     *  Negative numbers: the seconddate is before the firstdate
+     *  Positive numbers: the seconddate is behind the firstdate
+     *  Zero: firstdate is the same date as the seconddate
+     */
+    public static int getDaysBetween(Date firstdate, Date seconddate)
+    {
+        DateTime firstDate_joda = new DateTime(firstdate);
+        DateTime secondDate_joda = new DateTime(seconddate);
+        int daysBetween = Days.daysBetween(firstDate_joda.withTimeAtStartOfDay(),secondDate_joda.withTimeAtStartOfDay()).getDays();
+        return daysBetween;
     }
 }
--- app/src/main/java/com/github/kuma/grocerymanager/MealplanDataHandler.java
+++ app/src/main/java/com/github/kuma/grocerymanager/MealplanDataHandler.java
@@ -5,9 +5,6 @@ import com.github.kuma.data.db.DbDocument;
 import com.github.kuma.data.db.SimpleDbInterface;
 import com.github.kuma.db_object.Mealplan;
 
-import org.joda.time.DateTime;
-import org.joda.time.Days;
-
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
@@ -30,14 +27,6 @@ public class MealplanDataHandler
         validateData(); // remove any meal plan data before today
     }
 
-    public static int getDaysBetween(Date firstdate, Date seconddate)
-    {
-        DateTime firstDate_joda = new DateTime(firstdate);
-        DateTime secondDate_joda = new DateTime(seconddate);
-        int daysBetween = Days.daysBetween(firstDate_joda.withTimeAtStartOfDay(),secondDate_joda.withTimeAtStartOfDay()).getDays();
-        return daysBetween;
-    }
-
     private void validateData() throws Exception
     {
         if (data != null)
@@ -45,7 +34,7 @@ public class MealplanDataHandler
             for(DbDocument dbDoc : data)
             {
                 Date planDate = (Date) dbDoc.getProperty("date");
-                int daysBetween = getDaysBetween(today,planDate);
+                int daysBetween = DateUtils.getDaysBetween(today,planDate);
 
                 if(daysBetween < 0)
                 {
